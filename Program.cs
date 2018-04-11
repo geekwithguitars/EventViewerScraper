9249bc73473fc7a64aa00f3c3eab0cec54a5907e
Ramon Ecung <rje727@g.harvard.edu>
2018-04-11 08:42:37 -0400

    Currently processed line is now output for reach row. Added additional output in GetEventViewerLogs().

--- Program.cs
+++ Program.cs
@@ -33,6 +33,8 @@ namespace EventViewerScraper
             eventLog.Log = eventLogName;
             StringBuilder sb = new StringBuilder();
 
+            DisplayOutput("Now processing Event Viewer logs...");
+
             foreach (EventLogEntry log in eventLog.Entries)
             {
                 //DisplayOutput("Now processing: " + log.Message);
@@ -40,12 +42,14 @@ namespace EventViewerScraper
                 if (log.Source == Properties.Settings.Default.EventViewerSource && FilterMessageByTime(log.TimeWritten))
                 {
                     //EncodeString(log.Message);
-                    //DisplayOutput(log.Message);
+                    DisplayOutput("Now processing line: " + log.Message);
                     string line = log.TimeWritten.ToString("HH:mm")  + Properties.Settings.Default.delimiter + log.TimeWritten.ToString("D").Split(',')[0] + Properties.Settings.Default.delimiter + FormatMessage(log.Message) + Properties.Settings.Default.delimiter + isLogValid(log.Message);
                     DisplayOutput(line);
                     sb.AppendLine(line);
                 }
             }
+
+            DisplayOutput("Done processing Event Viewer logs!");
 
             return sb.ToString();
         }
@@ -88,6 +92,7 @@ namespace EventViewerScraper
         static void WriteFile (string textData , string path)
         {
             DisplayOutput("Attempting to write output file: " + path);
+
             try
             {
                 System.IO.File.WriteAllText(path, textData);
