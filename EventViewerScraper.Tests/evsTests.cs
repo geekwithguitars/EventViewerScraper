c345a9a92af5f5fa15ce93bf7533eb88b2af0c67
Ramon Ecung <rje727@g.harvard.edu>
2018-04-14 15:48:36 -0400

    Updating structure of the project
    I'm working with unit tests.

---
+++ EventViewerScraper.Tests/evsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using EventViewerScraper;
+
+namespace EventViewerScraper.Tests : EventViewerScraper.EventViewerScraper123
+{
+    public class evsTests
+    {
+        [TestFixture]
+        public class test1
+        {
+            EventViewerScraper123 asdf = new EventViewerScraper();
+
+            [Test]
+            public void Test12()
+            {
+                //Assert
+                Assert.That("string", Is.Not.Null.And.EqualTo("string"));
+
+            }
+        }
+    }
+}
